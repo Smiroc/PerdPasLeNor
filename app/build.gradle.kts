230060e703b24f4a85667a9edf49fdc1e3b9e652
matthieumarin.sio <matthieumarin.sio@gmail.com>
2023-12-06 17:38:40 +0100

    ajout de la verif de connexion pour affiché les données

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 }
