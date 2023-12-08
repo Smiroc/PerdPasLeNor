3f4bf044ee593e09090667f86d44d28441aad5d4
matthieumarin.sio <matthieumarin.sio@gmail.com>
2023-12-08 17:27:36 +0100

    ajout du code de quelqu'un qui a fait un projet appart

--- app/src/main/java/com/example/perdpaslenor/Activity.kt
+++ app/src/main/java/com/example/perdpaslenor/Activity.kt
@@ -2,15 +2,13 @@ package com.example.perdpaslenor
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, Authentification::class.java)
-
-        startActivity(intent)
     }
 }
