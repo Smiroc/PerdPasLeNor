1f6756fea28eeb28c2ee59a46b1633de6b798439
tatin <tatindylan@gmail.com>
2024-02-01 17:09:18 +0100

    ajout de la partie authentification

--- app/src/main/java/com/example/perdpaslenor/Authentification.kt
+++ app/src/main/java/com/example/perdpaslenor/Authentification.kt
@@ -4,24 +4,66 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Timer
+import java.util.TimerTask
 
 
 class Authentification : AppCompatActivity() {
     private var boutonconf: Button? = null
-
+    private var editcode : EditText? = null
+    private var nbr: Int = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.authentification)
 
+        val phoneTO = intent.getStringExtra("phoneTO")
+        val codeAUTH = intent.getStringExtra("codeAUTH")
+        val ME = intent.getStringExtra("ME")
+        val Genre = intent.getStringExtra("Genre")
+
+        if(Genre == "parent"){
+            IHMPARENT()
+        }else if(Genre == "enfant"){
+            IHMENFANT()
+        }
+
         boutonconf = findViewById<View>(R.id.buttonconfirmation) as Button?
-        boutonconf!!.setOnClickListener { Connexion() }
+
+        Toast.makeText(this, "Un code authentification a été envoyer", Toast.LENGTH_SHORT).show();
+
+        Chekup(ME, phoneTO ,codeAUTH)
+
+        val timer = Timer()
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                Handler(Looper.getMainLooper()).post {
+                    Toast.makeText(
+                        this@Authentification,
+                        "Il vous reste 1 minute pour rentrer le code reçu",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }, 60000)
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                TempsFIN()
+            }
+        }, 120000) // 120000 millisecondes = 2 minutes
 
         //Regarde si la permission de la localisation est accordée
         if (checkLocationPermission()) {
@@ -30,6 +72,93 @@ class Authentification : AppCompatActivity() {
         } else {
             // Demande la localisation si elle n'est pas active
             requestLocationPermission()
+        }
+
+        boutonconf!!.setOnClickListener { Parents(ME,phoneTO,codeAUTH) }
+    }
+
+    private fun TempsFIN(){
+        val intent = Intent(this, Connexion::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun Parents(ME : String?, phoneTO: String?, codeAUTH : String?){
+        editcode = findViewById(R.id.editionAUTH)
+        val etexte: String = editcode?.text.toString()
+        if(nbr > 0) {
+            if (codeAUTH == etexte) {
+                val internetPermission = ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.INTERNET
+                ) == PackageManager.PERMISSION_GRANTED
+
+                if(internetPermission) {
+                    // Établis la connexion avec la base de données
+                    val db = FirebaseFirestore.getInstance()
+
+                    // Créez un objet de type Map pour stocker vos données
+                    val user = hashMapOf(
+                        "numeroEnfant" to phoneTO,
+                        "numeroParent" to ME
+                    )
+
+                    // Ajoutez les données à votre collection "utilisateurs"
+                    db.collection("user")
+                        .add(user)
+                        .addOnSuccessListener { documentReference ->
+                            // L'ajout des données a réussi
+                            Toast.makeText(
+                                this,
+                                "L'ajout des données a réussi",
+                                Toast.LENGTH_SHORT
+                            ).show();
+                            IHMPARENT()
+                        }
+                        .addOnFailureListener { e ->
+                            // L'ajout des données a échoué
+                            Toast.makeText(
+                                this,
+                                "L'ajout des données a échoué",
+                                Toast.LENGTH_SHORT
+                            ).show();
+                        }
+                }else{
+                    Toast.makeText(
+                        this,
+                        "Veuillez autoriser la connexion à internet",
+                        Toast.LENGTH_SHORT
+                    ).show();
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Le code d'authentification ne correspond pas (il vous reste " + nbr + " essaie avant que le code ne soit plus valide)",
+                    Toast.LENGTH_SHORT
+                ).show();
+                nbr -= 1
+            }
+        }else{
+            val intent = Intent(this, Connexion::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun Chekup(ME : String?, phoneTO: String?, codeAUTH : String?){
+        var error = false
+        if(phoneTO ==  null){
+            error = true
+        }
+        if(codeAUTH ==  null){
+            error = true
+        }
+        if(ME ==  null){
+            error = true
+        }
+
+        if(error == true){
+            Toast.makeText(this, "Error réception de données", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -72,5 +201,17 @@ class Authentification : AppCompatActivity() {
                 requestLocationPermission()
             }
         }
+    }
+
+    private fun IHMPARENT(){
+        val intent = Intent(this, IHMParentReception::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun IHMENFANT(){
+        val intent = Intent(this, BackgroundChildrenService::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/perdpaslenor/Connexion.kt
+++ app/src/main/java/com/example/perdpaslenor/Connexion.kt
@@ -1,11 +1,125 @@
 package com.example.perdpaslenor
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.telephony.TelephonyManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class Connexion : AppCompatActivity() {
+    companion object {
+        const val PERMISSION_REQUEST_READ_PHONE_STATE = 1
+        const val PERMISSION_REQUEST_SEND_SMS = 101
+    }
+    private lateinit var number : EditText
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.connexion)
+
+        button = findViewById(R.id.Envoie)
+        number = findViewById(R.id.Number)
+
+
+
+        button.setOnClickListener{
+            val phone = number.text.toString()
+            if(phone.isNotEmpty()){
+                val randomNumber = (10000 .. 99999).random()
+
+                val phoneNumber = CheckPermission()
+
+                var obj=SmsManager.getDefault()
+                obj.sendTextMessage("$phone",null,"$randomNumber",
+                    null,null)
+                Authentification(phone, randomNumber,phoneNumber)
+            }
+        }
+    }
+
+    private fun CheckPermission(): String? {
+        var phoneNumber: String? = null // Initialisation avec null
+
+        var permissionSMS = false
+        var permissionNUM = false
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // Demander la permission d'envoyer des SMS
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), PERMISSION_REQUEST_SEND_SMS)
+        } else {
+            permissionSMS = true
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // Demander la permission d'accéder au numéro de téléphone
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), PERMISSION_REQUEST_READ_PHONE_STATE)
+        } else {
+            permissionNUM = true
+        }
+
+        if (permissionNUM && permissionSMS) {
+            // Les deux permissions sont accordées
+            // Vous pouvez maintenant accéder au numéro de téléphone
+            phoneNumber = getPhoneNumber()
+        } else {
+            // Vous devrez attendre la réponse de l'utilisateur dans onRequestPermissionsResult()
+        }
+
+        return phoneNumber
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_SEND_SMS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission d'envoi de SMS accordée
+                    // Vous pouvez effectuer des actions nécessitant cette permission
+                } else {
+                    // La permission d'envoi de SMS a été refusée
+                    Toast.makeText(this, "Veuillez accepter la permission d'envoie sms", Toast.LENGTH_SHORT).show();
+                }
+            }
+            PERMISSION_REQUEST_READ_PHONE_STATE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission d'accès au numéro de téléphone accordée
+                    // Vous pouvez effectuer des actions nécessitant cette permission
+                } else {
+                    // La permission d'accès au numéro de téléphone a été refusée
+                    Toast.makeText(this, "Veuillez accepter la permission information sur le numéro de téléphone", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    private fun getPhoneNumber(): String? {
+        return if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            telephonyManager.line1Number?.takeIf { it.isNotBlank() }
+        } else {
+            // La permission n'est pas accordée
+            // Gérer le cas où l'application n'a pas la permission d'accéder au numéro de téléphone
+            null
+        }
+    }
+
+
+
+
+    private fun Authentification(phone : String,  randomNumber: Int, phoneNumber: String?) {
+        val intent = Intent(this, Authentification::class.java)
+        val chaine: String = randomNumber.toString()
+        intent.putExtra("phoneTO", phone);
+        intent.putExtra("codeAUTH", chaine);
+        intent.putExtra("ME", phoneNumber);
+        startActivity(intent)
+        finish()
     }
 }
